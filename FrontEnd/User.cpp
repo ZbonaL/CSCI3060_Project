04a44a4a2ec7f2b5fe98ec53a22470fe4bc30572
Leo <leonard.zbona@uoit.net>
2019-02-10 12:30:18 -0500

    Added new class files

--- FrontEnd/User.cpp
+++ FrontEnd/User.cpp
@@ -3,4 +3,13 @@
 
 using namespace std;
 
-User::User(string username, )
+// user constructor
+User::User(string username, string accountType, double creditAmount){
+    SetUser(username, accountType, creditAmount);
+}
+
+void User::SetUser(string username, string accountType, double creditAmmount){
+    new_username = username;
+    new_accountType = accountType;
+    new_creditAmount = creditAmmount;
+}
--- FrontEnd/User.h
+++ FrontEnd/User.h
@@ -5,16 +5,16 @@ using namespace std;
 class User{
 
   private:
-  string username;
-  string accountType;
-  double creditAmount;
+  string new_username;
+  string new_accountType;
+  double new_creditAmount;
 
   public:
     User(string username, string accountType, double creditAmount);
 
     void SetUser(string username, string accountType, double creditAmount);
     
-    string getUserName(){ return username; }
-    string getAccountType(){ return accountType; }
-    double getCreditAmount(){ return creditAmount; }
+    string getUserName(){ return new_username; }
+    string getAccountType(){ return new_accountType; }
+    double getCreditAmount(){ return new_creditAmount; }
 }
