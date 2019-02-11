99ce2c52d2e98db8278537ecec288e4f4b6f36fe
Sinthooran Ravinathan <sinthooran.ravinathan@uoit.net>
2019-02-10 19:08:02 -0500

    Adding buy and login features

--- FrontEnd/Transaction.cpp
+++ FrontEnd/Transaction.cpp
@@ -1,8 +1,13 @@
 #include <Transaction.h>
 #include <iostream>
 #include <string>
+#include <User.h>
+#include <Ticket.h>
 
 using std::string;
+
+
+
 
 //Grabs the transaction from the methods below 
 void getTransactionID(){
@@ -11,22 +16,28 @@ void getTransactionID(){
 
 //Transaction Methods 
 // Login #TODO
-void login(){
+void login(string userName){
+    std::cout << "Enter a Username: ";
+    std::cin >> userName;
+
+    while(userName.size() == 0 || userName.size() == 15){
+        std::cout << "Was not Correct. Please Try Again" << std::endl;
+
+    }
 
 }
 
 // Logout #TODO
 void logout(){
-
 }
 
 // Create
-void createUser(){
+void createUser(string userName, string accountType, double credit){
 
 }
 
 // Delete
-void deleteUser(){
+void deleteUser(string userName){
 
 }
 
@@ -36,8 +47,17 @@ void sell(){
 }
 
 // Buy #TODO
-void buy(){
-
+void buy(string eventname, string sellername, int ticketQuantity){
+    std::cout << "Enter the Login of the Event: ";
+    std::cin >> eventname;
+    if (eventname.size() == 0 || eventname.size() > 25){
+        std::cout << "Sorry that was an invalid event: ";
+        //logout();
+    }
+    else{
+        std::cout << "Enter the Seller's Username: ";
+        std::cin >> sellername;
+    }
 }
 
 // Refund
