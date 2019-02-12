fef078be5f22a5011d33e8c1fa577c494248e252
Danooshan Sureshkumar <danooshan0919@gmail.com>
2019-02-11 19:11:39 -0500

    Wrote comments for User header and cpp

--- FrontEnd/User.cpp
+++ FrontEnd/User.cpp
@@ -1,16 +1,32 @@
-#include "User.h"
+/*
+This is a program that holds the definitions of the methods and classes defined in User.h
+
+@authors: Leonard Zbona, Sinthooran Ravinathan, Danooshan Sureshkumar
+@since: 2019-01-14
+@version 1.0
+*/
+
+#include "User.h" //Includes the declaration of the User class and its methods.
 #include <string>
 
 using std::string;
 
-// user constructor
+//Method: User
 User::User(string UserName, string accountType, double creditAmount){
+	//"this" refers to the instance of the duplicate variables belonging to the Object
     this->UserName = UserName;
     this->accountType = accountType;
     this->creditAmount = creditAmount;
 }
 
+//Method: getUserName
 string User::getUserName(){ return UserName; }
+
+//Method: getAccountType
 string User::getAccountType(){ return accountType; }
+
+//Method: getCreditAmount
 double User::getCreditAmount(){ return creditAmount; }
+
+//Method: setCredit
 void User::setCredit(double credit){ creditAmount = credit; }
--- FrontEnd/User.h
+++ FrontEnd/User.h
@@ -1,7 +1,21 @@
-#include <string>
-#ifndef USER_H_
-#define USER_H_
+/*
+This is a header file which defines the User class, and declares all classes and methods.
 
+@authors: Leonard Zbona, Sinthooran Ravinathan, Danooshan Sureshkumar
+@since: 2019-01-14
+@version 1.0
+*/
+
+#include <string>
+#ifndef USER_H_	//If a User.h header file is not already included:
+#define USER_H_ //Include the User.h class
+
+/*
+Class name: User
+Description: A User class for creating User objects for our ticket selling program.
+			 Users have public instance variables: UserName (string), accountType (string). And 
+			 private instance variables: creditAmount (double).
+*/
 class User{
 
   private:
@@ -10,10 +24,25 @@ class User{
   public:
     std::string UserName;
  	std::string accountType;
+	
+	//Method Name: User
+	//Purpose: Constructs the User object.
     User(std::string UserName, std::string accountType, double creditAmount);
+	
+	//Method Name: getUserName
+	//Purpose: Returns the UserName of the User object.
 	std::string getUserName();
+
+	//Method Name: getAccountType
+	//Purpose: Returns the accountType of the User object.
     std::string getAccountType();
+	
+	//Method Name: getCreditAmount
+	//Purpose: Returns the creditAmount of the User object.
     double getCreditAmount();
+
+	//Method Name: setCredit
+	//Purpose: Used for altering the value of credit.
 	void setCredit(double credit);
 };
 #endif
