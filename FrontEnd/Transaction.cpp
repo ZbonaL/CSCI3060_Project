183d57f1f262687461b727c540b8ca488116e828
Danooshan Sureshkumar <danooshan0919@gmail.com>
2019-02-11 19:58:35 -0500

    Added comments and documentation to Transaction header and cpp files

--- FrontEnd/Transaction.cpp
+++ FrontEnd/Transaction.cpp
@@ -1,21 +1,29 @@
+/*
+This is a program that holds the definitions of the methods and classes defined in Transaction.h
+
+@authors: Leonard Zbona, Sinthooran Ravinathan, Danooshan Sureshkumar
+@since: 2019-01-14
+@version 1.0
+*/
 #include <iostream>
-#include "User.h"
-#include "Transaction.h"
+#include "User.h" //Includes the declaration of the User class and its methods.
+#include "Transaction.h" //Includes the declaration of the Transaction class and its methods.
 #include <string>
 #include <fstream>
 
-
+//Importing from the standard library in order to avoid using redundant methods and slowing down performance.
 using std::string;
 using std::cout;
 using std::cin;
 using std::endl;
 
+//Method: Transaction
 Transaction::Transaction(){
 
 }
 
 //Transaction Methods 
-// Login #TODO
+//Method: login
 void Transaction::login(string UserName, string temp, bool nameExists){
     
     nameExists = 0;
@@ -34,8 +42,8 @@ void Transaction::login(string UserName, string temp, bool nameExists){
   	}
 }
 
-// Logout #TODO
-void logout(){
+//Method: logout
+void Transaction::logout(){
     string filename;
     string output;
     
@@ -46,28 +54,28 @@ void logout(){
     exit(0);
 }
 
-// Create
-void create(string userName, string accountType, double credit){
+//Method: create
+void Transaction::create(string userName, string accountType, double credit){
 
 }
 
+//Method: deleteUser
 // Delete Note: Used deleteUser instead of delete due to delete being a C++ keyword
-void deleteUser(string userName){
+void Transaction::deleteUser(string userName){
 
 }
 
-// Sell
-void sell(){
+//Method: sell
+void Transaction::sell(){
 
 }
 
-// Buy #TODO
-void buy(string eventname, string sellername, int ticketQuantity){
+//Method: buy
+void Transaction::buy(string eventname, string sellername, int ticketQuantity){
     cout << "Enter the Login of the Event: ";
     cin >> eventname;
     if (eventname.size() == 0 || eventname.size() > 25){
         cout << "Sorry that was an invalid event: ";
-        //logout();
     }
     else{
         cout << "Enter the Seller's Username: ";
@@ -75,12 +83,12 @@ void buy(string eventname, string sellername, int ticketQuantity){
     }
 }
 
-// Refund
-void refund(){
+//Method: refund
+void Transaction::refund(){
 
 }
 
-// AddCredit
-void addcredit(){
+//Method: addCredit
+void Transaction::addcredit(){
 
 }
--- FrontEnd/Transaction.h
+++ FrontEnd/Transaction.h
@@ -1,17 +1,56 @@
-#ifndef TRANSACTION_H_
-#define TRANSACTION_H_
+/*
+This is a header file which defines the Transaction class, and declares all classes and methods used by Transaction objects.
 
+@authors: Leonard Zbona, Sinthooran Ravinathan, Danooshan Sureshkumar
+@since: 2019-01-14
+@version 1.0
+*/
+
+#ifndef TRANSACTION_H_ //If a Transaction.h header file is not already included:
+#define TRANSACTION_H_ //Include the Transaction.h class
+
+/*
+Class name: Transaction
+Description: A Transaction class for creating Transaction objects for our ticket selling program.
+*/
 class Transaction{
 
 	public:
+
+		//Method Name: Transaction
+		//Purpose: Constructs the Transaction object.
 		Transaction();
+
+		//Method Name: login
+		//Purpose: Compares the inputted UserName to the usernames in the Users file to check if it exists.
 		void login(std::string UserName, std::string temp, bool nameExists);
+
+		//Method Name: logout
+		//Purpose: Acts as an ideal exit to the program, writes the recorded transactions to the output file.
 		void logout();
+	
+		//Method Name: buy
+		//Purpose: Purchases a number of available tickets and transfers credit from the buying user to the selling user.
 		void buy();
+		
+		//Method Name: sell
+		//Purpose: Sells a number of tickets and writes them to the tickets file to be available for purchase.
 		void sell();
+
+		//Method Name: create
+		//Purpose: Creates a User class, and issues them a privelege. Then saves this to the current user input file. 
 		void create();
+	
+		//Method Name: deleteUser
+		//Purpose: Cancels any outstanding tickets for purchase or sale of a user and removes that user account.
 		void deleteUser();
+
+		//Method Name: refund
+		//Purpose: Issues a credit from the buyers account to the sellers account.
 		void refund();
+
+		//Method Name: addCredit
+		//Purpose: Adds credit to a users own account or as an admin adds credit to a different user account
 		void addCredit();
 };
-#endif
+#endif // Ends the ifndef condition
