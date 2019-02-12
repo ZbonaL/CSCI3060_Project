71e17bd08532775cae9af304859a1efcf23c39b2
Danooshan Sureshkumar <danooshan0919@gmail.com>
2019-02-11 20:12:51 -0500

    Added comments/documentation to the ticket header and cpp file

--- FrontEnd/Ticket.cpp
+++ FrontEnd/Ticket.cpp
@@ -1,14 +1,27 @@
-#include "Ticket.h"
+/*
+This is a program that holds the definitions of the methods and classes defined in Ticket.h
+
+@authors: Leonard Zbona, Sinthooran Ravinathan, Danooshan Sureshkumar
+@since: 2019-01-14
+@version 1.0
+*/
+#include "Ticket.h" //Includes the declaration of the Ticket class and its methods.
 #include <string>
 
 using std::string;
 
+//Method: Ticket
 Ticket::Ticket(string EventTitle, int TicketQuantity, double TicketPrice){
     this->EventTitle = EventTitle;
     this->TicketQuantity = TicketQuantity;
     this->TicketPrice = TicketPrice;
 }
 
+//Method: getEventTitle
 string Ticket::getEventTitle(){ return EventTitle; }
+
+//Method: getTicketQuantity
 int Ticket::getTicketQuantity(){ return TicketQuantity; }
+
+//Method: getTicketPrice
 double Ticket::getTicketPrice(){ return TicketPrice; }
--- FrontEnd/Ticket.h
+++ FrontEnd/Ticket.h
@@ -1,16 +1,36 @@
-#include <string>
-#ifndef TICKET_H_
-#define TICKET_H_
+/*
+This is a header file which defines the Ticket class, and declares all classes and methods used by Ticket objects.
 
+@authors: Leonard Zbona, Sinthooran Ravinathan, Danooshan Sureshkumar
+@since: 2019-01-14
+@version 1.0
+*/
+#include <string>
+#ifndef TICKET_H_ //If a Ticket.h header file is not already included:
+#define TICKET_H_ //Include the Ticket.h class
+
+//Class name: Ticket
+//Definition: A Ticket class for creating Transaction objects for our ticket selling program.
 class Ticket{
     public:
         std::string EventTitle;
         int TicketQuantity;
         double TicketPrice;
 
+		//Method name: Ticket
+		//Purpose: Constructs a Ticket object with an EventTitle, TicketQuantity, and TicketPrice
         Ticket(std::string EventTitle, int TicketQuantity, double TicketPrice);
+
+		//Method name: getEventTitle
+		//Purpose: Returns the EventTitle of the Ticket object.
         std::string getEventTitle();
+
+		//Method name: getTicketQuantity
+		//Purpose: Returns the TicketQuantity of the Ticket object.
         int getTicketQuantity();
+
+		//Method name: getTicketPrice
+		//Purpose: Returns the TicketPrice of the Ticket object.
         double getTicketPrice(); 
 };
-#endif 
+#endif // Ends the ifndef condition
