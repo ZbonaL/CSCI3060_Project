b58c29c5b64be772210f4d42690aa02cf6bdbd20
Leo <leonard.zbona@uoit.net>
2019-03-03 01:21:05 -0500

    Made script loops through both input and output

--- FrontEnd/script.sh
+++ FrontEnd/script.sh
@@ -2,13 +2,25 @@ touch out.out
 touch tempTran.trn
 for t in "LoginInput" "LogoutInputs" "AddCreditInputs" "BuyInput" "CreateInput" "DeleteInput" "RefundInput" "SellInput"
   do
-	echo "Running tests for: $t"
-	for i in ../Tests/TestInputs/$t/*.input
-	  do
-		# echo "${i%.input}"
-		echo "$i"
-		# cat "$i"
-		./output < $i
-    done    
+		echo "Running tests for: $t"
+		for i in ../Tests/TestInputs/$t/*.input
+	  	do
+				#echo "$i"
+				# cat "$i"
+				cat "$i" | ./output tempTran > out.out
+				for j in "LoginOutput" "LogoutOutput" "AddCreditOutputs" "BuyOutput" "CreateOutput" "DeleteOutput" "RefundOutput" "SellOutput"
+					do
+						for h in ../Tests/Results/$j/*.output
+							do
+								diff out.out "${h%}"
+								rm out.out
+								rm tempTran.trn
+								touch out.out
+								touch tempTran.trn
+						done
+				done
+		done    
 done
 
+rm out.out
+rm tempTran.trn
