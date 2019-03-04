93ee6db54d809bcb192fe6e0edd47c000f0d4bc2
Sinthooran Ravinathan <sinthooran.ravinathan@uoit.net>
2019-03-03 19:42:12 -0500

    Fixed Up Test Cases Files Input/Outputs based off code

--- FrontEnd/script.sh
+++ FrontEnd/script.sh
@@ -2,38 +2,41 @@ touch out.out
 touch tempTran.trn
 # this first loop goes throught all the test inpuput folders
 for t in "AddCreditInputs" "BuyInput" "CreateInput" "DeleteInput" "LoginInput" "LogoutInputs" "RefundInput" "SellInput"
-	do
-		echo "Running tests for: $t"
-		#this loop goes through the actual inputs and adds them,
-		for i in ../Tests/TestInputs/$t/*.input
-	  		do
-				#echo "$i"
-				# cat "$i"
-				echo "${i%.input}" > tempTran.trn
-				#input the data from the input files into the c++ program
-				cat "$i" | ./output tempTran.trn > out.out
-		done    
+    do
+        echo "Running tests for: $t"
+        #this loop goes through the actual inputs and adds them,
+        for i in ../Tests/TestInputs/$t/*.input
+              do
+                #echo "$i"
+                # cat "$i"
+                echo "${i%.input}" 
+                #input the data from the input files into the c++ program
+                ./output   $i > out.out
+                rm out.out
+                rm tempTran.trn
+                touch out.out
+                touch tempTran.trn 
+        done
+
+    #loop through all the folders in the output dir
+    for j in "AddCreditOutputs" "BuyOutput" "CreateOutput" "DeleteOutput" "LoginOutput" "LogoutOutput" "RefundOutput" "SellOutput"
+        do    
+            echo "Checking tests for: $j"
+            #loop through each of the test outputs 
+            for h in ../Tests/Results/$j/*.output
+                do    
+                    #echo "$out.out"
+                    #check if the out.out file is the same as the test outputs
+                    if diff out.out $h;
+                    then 
+                        echo "test $j failed"
+                    else
+                        echo "test $j good"
+                    fi
+            done
+    done
+    
 done
-#loop through all the folders in the output dir
-for j in "AddCreditOutputs" "BuyOutput" "CreateOutput" "DeleteOutput" "LoginOutput" "LogoutOutput" "RefundOutput" "SellOutput"
-	do	
-		echo "Checking tests for: $j"
-		#loop through each of the test outputs 
-		for h in ../Tests/Results/$j/*.output
-			do	
-				#echo "$out.out"
-				#check if the out.out file is the same as the test outputs
-				if diff out.out $h;
-				then 
-					echo "test $j good"
-				else
-					echo "test $j failed"
-				fi
-				rm out.out
-				rm tempTran.trn
-				touch out.out
-				touch tempTran.trn
-		done
-done
+
 rm out.out
 rm tempTran.trn
